34a4cefcdc2157cb4f091fe07de68f6a2d543c6f
Pedro Carvi <pedro.carvi-11@outlook.es>
2024-06-22 19:13:18 -0300

    Reviews design + layout mini  changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import AsusTuf1 from './assets/asus_tuf.png'
 import GoogleReviews from './components/GoogleReviews/GoogleReviews';
 import ProductLayoutOne from './components/ProductLayoutOne/ProductLayoutOne';
 import ContactForm from './components/ContactForm/ContactForm';
+import LocationIcon from './assets/location.png'
+import PhoneIcon from './assets/call.png'
+import MailIcon from './assets/sms-edit.png'
 
 function App() {
   return (
@@ -48,7 +51,7 @@ function App() {
         description='Con Intel Core i9 y NVIDIA GeForce RTX 4060, ofrece un rendimiento excepcional para juegos exigentes. 32GB de RAM y SSD de 1TB aseguran velocidad y capacidad.'
         imageSrc={AsusTuf1}
       />
-      <div className="section reviews">
+      <div className="section-reviews">
         <div className="section-sub-container">
           <h2> Ellos ya confiaron en Mixerport </h2>
           <h3> Testimonios de nuestros clientes </h3>
@@ -64,20 +67,20 @@ function App() {
           <ContactForm />
         </div>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6568.169847147494!2d-58.37316200000001!3d-34.602014!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccbf5082dee1d%3A0x87396e43d668cc41!2sMixerport%20Importaciones!5e0!3m2!1ses!2sar!4v1718992688974!5m2!1ses!2sar" style={{ width: '100%', height: '400px' }} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6568.169847147494!2d-58.37316200000001!3d-34.602014!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccbf5082dee1d%3A0x87396e43d668cc41!2sMixerport%20Importaciones!5e0!3m2!1ses!2sar!4v1718992688974!5m2!1ses!2sar" className='maps-iframe' loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <div className="footer">
         <div className="footer-sub-container">
           <img src={LogoImage} alt="logo-mixer" width={140} />
           <div className="icon-box direccion">
-            <img src={LogoImage} alt="" width={20} />
+            <img src={LocationIcon} alt="" width={20} />
             <p> Lavalle 445, 3er piso, CABA</p>
           </div>
           <div className="icon-box telefono">
-            <img src={LogoImage} alt="" width={20} />
-            <p> Lavalle 445, 3er piso, CABA</p>
+            <img src={PhoneIcon} alt="" width={20} />
+            <p> +54 11 2885-9227</p>
           </div>
           <div className="icon-box direccion">
-            <img src={LogoImage} alt="" width={20} />
+            <img src={MailIcon} alt="" width={20} />
             <p> ventas@mixerport.com </p>
           </div>
         </div>
--- src/components/GoogleReviews/GoogleReviews.jsx
+++ src/components/GoogleReviews/GoogleReviews.jsx
@@ -3,10 +3,14 @@ import axios from 'axios';
 import './GoogleReviews.css';
 import GoogleLogo from '../../assets/google_logo.png'
 import GoogleLogoSmall from '../../assets/google_logo_s.png'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const GoogleReviews = () => {
     const [reviews, setReviews] = useState([]);
     const [expanded, setExpanded] = useState({});
+    const [totalReviews, setTotalReviews] = useState(0);
 
     useEffect(() => {
         const fetchReviews = async () => {
@@ -14,6 +18,7 @@ const GoogleReviews = () => {
                 const response = await axios.get('https://express-mixer-land.vercel.app/api/place-details');
                 if (response) {
                     setReviews(response.data.reviews);
+                    setTotalReviews(response.data.user_ratings_total);
                 }
             } catch (error) {
                 console.error('Error fetching reviews:', error);
@@ -42,48 +47,74 @@ const GoogleReviews = () => {
         return text.substring(0, length) + "...";
     };
 
+    const settings = {
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
+
     return (
         <div className="reviews-container">
             <div className="review-summary">
-                <h2>Excelente</h2>
+                <h2 style={{ fontSize: '24px', fontWeight: 'bold', color: 'white', textTransform: 'none' }}>Excelente</h2>
                 <div className="review-summary-stars">
                     {renderStars(5)}
                 </div>
-                <p>A base de <strong>559 reseñas</strong></p>
+                <p>A base de <strong> {totalReviews ? `${totalReviews} reseñas` : '+500 reseñas'}</strong></p>
                 <img src={GoogleLogo} alt="Google" className="google-logo" width={160} />
             </div>
             <div className="review-reviews">
                 {
                     reviews.length > 0 ? (
                         <div className='review-reviews-subctn'>
-                            {
-                                reviews.map((review, index) => (
-                                    <div key={index} className="review-card">
-                                        <div className="review-header">
-                                            <div className='review-header-subctn'>
-                                                <img src={review.profile_photo_url} alt={review.author_name} className="review-avatar" />
-                                                <div className='review-name-rating-ctn'>
-                                                    <h3>{review.author_name}</h3>
-                                                    <p className="review-date">{new Date(review.time * 1000).toLocaleDateString()}</p>
+                            <Slider {...settings}>
+                                {
+                                    reviews.map((review, index) => (
+                                        <div key={index} className="review-card">
+                                            <div className="review-header">
+                                                <div className='review-header-subctn'>
+                                                    <img src={review.profile_photo_url} alt={review.author_name} className="review-avatar" />
+                                                    <div className='review-name-rating-ctn'>
+                                                        <h3>{review.author_name}</h3>
+                                                        <p className="review-date">{new Date(review.time * 1000).toLocaleDateString()}</p>
+                                                    </div>
                                                 </div>
+                                                <img src={GoogleLogoSmall} alt="Google" width={25} />
                                             </div>
-                                            <img src={GoogleLogoSmall} alt="Google" width={25} />
+                                            <div className="review-stars">{renderStars(review.rating)}</div>
+                                            <p className="review-text">
+                                                {expanded[index] ? review.text : truncateText(review.text, 150)}
+                                                {review.text.length > 150 && (
+                                                    <span style={{ fontSize: '14px', cursor: 'pointer', color: 'rgba(255,255,255,0.8)' }} onClick={() => toggleExpanded(index)}>
+                                                        {expanded[index] ? "Leer menos" : "Leer más"}
+                                                    </span>
+                                                )}
+                                            </p>
                                         </div>
-                                        <div className="review-stars">{renderStars(review.rating)}</div>
-                                        <p className="review-text">
-                                            {expanded[index] ? review.text : truncateText(review.text, 100)}
-                                            {review.text.length > 100 && (
-                                                <button onClick={() => toggleExpanded(index)}>
-                                                    {expanded[index] ? "Leer menos" : "Leer más"}
-                                                </button>
-                                            )}
-                                        </p>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                }
+                            </Slider>
                         </div>
                     ) : (
-                        <p>No reviews</p>
+                        <p>No reviews found.</p>
                     )
                 }
             </div>
